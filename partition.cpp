42fe601aee748f9385c1dd6a0153076ba64221c2
Eryk <eryk.pecyna@gmail.com>
2020-04-27 15:47:28 +0000

    most done (not tested well) but not simulated annealing

--- partition.cpp
+++ partition.cpp
@@ -6,6 +6,7 @@
 #include <algorithm>
 #include <random>
 #include <chrono>
+#include <math.h>
 
 #define MAXLINE 256
 
@@ -13,23 +14,47 @@ using namespace std;
 
 struct Numpartset {
 	vector<int> nums;
-	// vector<int> prepart;
-	
-
+	vector<int> prepart;
+	vector<int> combined;
+	int length;
+	int best;
 
 	Numpartset(char* filename) {
 		ifstream infile(filename, ios::in);
 		char buff[MAXLINE];
 
 		infile.getline(buff, MAXLINE);
-		for (int _ = 0; _ < 100; _++) {
+		// for (int _ = 0; _ < 100; _++) {
+		while(!infile.eof()) {
 			nums.push_back(stoi(buff));
 			infile.getline(buff, MAXLINE);
 		}
 
 		sort(nums.begin(), nums.end());
+
+		length = nums.size();
 	}
 
+	bool combine() {
+		combined.erase(combined.begin(), combined.end());
+
+		for(int _ = 0; _ < length; _++) {
+			combined.push_back(0);
+		}
+
+		for(int i = 0; i < length; i++) 
+			combined[prepart[i]] += nums[i];
+
+		int res = KK_2();
+
+		if (best > res) {
+			best = res;
+			return true;
+		}
+		return false;
+	}
+
+	// Using list in nums, usually original list
 	int KK() {
 		while(nums.size() > 1) {
 			int n = nums.size();
@@ -46,10 +71,182 @@ struct Numpartset {
 		return(nums[0]);
 	}
 
-	int RR() {
+	// Using prepartition combined list
+	int KK_2() {
+		while(combined.size() > 1) {
+			int n = combined.size();
+			int a = combined[n-1];
+			int b = combined[n-2];
+			int c = abs(a-b);
+
+			combined.pop_back();
+			combined[n-2] = c;
+
+			sort(combined.begin(), combined.end());
+		}
+
+		return(combined[0]);
+	}
+
+	// Check residue given a sequence of +1/-1
+	bool check() {
+		int res = 0;
+		
+		for(int i = 0; i < length; i++) {
+			res += prepart[i] * nums[i];
+		}
+		
+		if (best > res) {
+			best = res;
+			return true;
+		}
+		return false;
+	}
+
+	// Repeated Random
+	int RR(int max_iters) {
 		unsigned seed = chrono::system_clock::now().time_since_epoch().count();
     	mt19937 generator(seed);
-    	uniform_real_distribution<float> unif(0,1);
+    	bernoulli_distribution bern(0.5);
+		// Random solution S
+		for(int _ = 0; _ < 100; _++) {
+			prepart.push_back(bern(generator) ? 1 : -1);
+		}
+		check();
+		for(int _ = 0; _ < max_iters; _ ++) {
+			// New solution S'
+			for(int i = 0; i < 100; i++) {
+				prepart[i] = bern(generator) ? 1 : -1;
+			}
+			// Check if res(S') < res(S) and update if so
+			check();
+		}
+
+		return best;
+	}
+
+	// Prepartitioned Repeated Random
+	int P_RR(int max_iters) {
+		unsigned seed = chrono::system_clock::now().time_since_epoch().count();
+    	mt19937 generator(seed);
+    	uniform_int_distribution<int> unif(1,100);
+		// Random solution S
+		for(int _ = 0; _ < 100; _++) {
+			prepart.push_back(unif(generator));
+		}
+		combine();
+		for(int _ = 0; _ < max_iters; _ ++) {
+			// New solution S'
+			for(int i = 0; i < 100; i++) {
+				prepart[i] = unif(generator);
+			}
+			// Check if res(S') < res(S) and update if so
+			combine();
+		}
+
+		return best;
+	}
+
+	// Hill Climbing
+	int HC(int max_iters) {
+		unsigned seed = chrono::system_clock::now().time_since_epoch().count();
+    	mt19937 generator(seed);
+    	bernoulli_distribution bern(0.5);
+		uniform_int_distribution<int> randind(1,100);
+		// Random solution S
+		for(int _ = 0; _ < 100; _++) {
+			prepart.push_back(bern(generator) ? 1 : -1);
+		}
+		check();
+
+		for(int _ = 0; _ < max_iters; _ ++) {
+			// Random neighbor S'
+			int i = randind(generator);
+			int j = randind(generator);
+			while (i == j)
+				j = randind(generator);
+
+			int old_i = prepart[i];
+			int old_j = prepart[j];
+
+			prepart[i] *= -1;
+			if (bern(generator)) {
+				prepart[j] *= -1;
+			}
+
+			if(!check()) {
+				prepart[i] = old_i;
+				prepart[j] = old_j;
+			}
+		}
+
+		return best;
+	}
+
+	// Prepartitioned Hill Climbing
+	int P_HC(int max_iters) {
+		unsigned seed = chrono::system_clock::now().time_since_epoch().count();
+    	mt19937 generator(seed);
+		uniform_int_distribution<int> randind(1,100);
+		// Random solution S
+		for(int _ = 0; _ < 100; _++) {
+			prepart.push_back(randind(generator));
+		}
+		combine();
+		
+		for(int _ = 0; _ < max_iters; _ ++) {
+			// Random neighbor S'
+			int i = randind(generator);
+			int j = randind(generator);
+			int old_i = prepart[i];
+
+			while(prepart[i] == j) {
+				j = randind(generator);
+			}
+
+			prepart[i] = j;
+
+			if(!combine()) {
+				prepart[i] = old_i;
+			}
+		}
+
+		return best;
+	}
+
+	double T(int iter) {
+		return(pow(10,10) * pow(0.8, floor(iter/300)));
+	}
+
+	int SA(int max_iters) {
+		unsigned seed = chrono::system_clock::now().time_since_epoch().count();
+    	mt19937 generator(seed);
+    	uniform_int_distribution<int> unif(0,1);
+		uniform_int_distribution<int> randind(1,100);
+		// Random solution S
+		for(int _ = 0; _ < 100; _++) {
+			prepart.push_back(unif(generator));
+		}
+		best = check();
+
+		for(int _ = 0; _ < max_iters; _ ++) {
+			// Random neighbor S'
+			int i = randind(generator);
+			int j = randind(generator);
+			int old_i = prepart[i];
+
+			while(prepart[i] == j) {
+				j = randind(generator);
+			}
+			
+			prepart[i] = j;
+			int res_S = check();
+			if(res_S >= best) {
+				prepart[i] = old_i;
+			} else {
+				best = res_S;
+			}
+		}
 	}
 };
 
@@ -65,14 +262,18 @@ int main(int argc, char* argv[]) {
 			cout << test.KK() << endl;
 			break;
 		case 1:
+			cout << test.RR(25000) << endl;
 			break;
 		case 2:
+			cout << test.HC(25000) << endl;
 			break;
 		case 3:
 			break;
 		case 11:
+			cout << test.P_RR(25000) << endl;
 			break;
 		case 12:
+			cout << test.P_HC(25000) << endl;
 			break;
 		case 13:
 			break;
